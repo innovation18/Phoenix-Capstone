14ddacc3bc39fd842ebadad7d4be715f3766f9ec
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2022-01-03 18:34:18 +0530

    https://stackoverflow.com/questions/10107350/how-to-handle-the-pylint-message-idw0612-unused-variable

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def create_app(name: str = 'Hanish Arora'):
     app = Flask(__name__)
 
     @app.route("/")
-    def index():
+    def dummy():  # https://stackoverflow.com/questions/10107350/how-to-handle-the-pylint-message-idw0612-unused-variable
         return f"Hello World, This is sample application deployed by {name} for Udacity DevOps Capstone! Happy Coding!"
 
     return app
