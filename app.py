ea1f8fee14289863a1504213d84cf26f52398522
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2022-01-05 21:19:09 +0530

    Corrected - Versioning

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def create_app(name: str = 'Hanish Arora'):
 
     @app.route("/")
     def dummy():  # https://stackoverflow.com/questions/10107350/how-to-handle-the-pylint-message-idw0612-unused-variable
-        return f"Hello World!, This is sample application Version:VERSION from deployed by {name} for Udacity DevOps Capstone! Happy Coding!"
+        return f"Hello World!, This is sample application V1:VERSION from deployed by {name} for Udacity DevOps Capstone! Happy Coding!"
 
     return app
